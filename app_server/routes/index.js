0763d48ac3c2d4413b445e5c9ac6957ef9baa6a3
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-04-28 09:13:37 -0400

    Set up location pages

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
 
-var homepageController = function (req, res) {
- res.render('index', { title: 'Express' });
-}; 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.home);
+router.get('/location', ctrlLocations.locationInfo)
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
 
 module.exports = router;
