bc78fa7e9826fa2bba15478bf72108a0ecaf1464
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-04-28 09:16:12 -0400

    renamed main.js to other.js and finished adding routes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -10,5 +10,6 @@ router.get('/location', ctrlLocations.locationInfo)
 router.get('/location/review/new', ctrlLocations.addReview);
 
 /* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
