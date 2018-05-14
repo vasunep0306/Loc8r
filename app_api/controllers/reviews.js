af59e158ebb11f2672b24f05fff5642c9f59daf5
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-14 16:56:04 -0400

    Updating a subdocument in MongoDB

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -130,8 +130,56 @@ module.exports.reviewsReadOne = function(req,res) {
 
 //router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
 module.exports.reviewsUpdateOne = function(req,res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    if ( !req.params.locationid || !req.params.reviewid ) {
+        sendJsonResponse(res, 400, {
+            "message": "Not found, locationid and reviewid are both required"
+        });
+        return;
+    }
+    //find parent document
+    Loc
+    .findById( req.params.locationid )
+    .select( 'reviews' )
+    .exec ( 
+        function( err, location ) {
+            var thisReview;
+            if ( !location ) {
+                sendJsonResponse(res, 404, {
+                    "message": "locationid not found"
+                });
+                return;
+            } else if ( err ) {
+                sendJsonResponse(res, 400, err);
+                return;
+            } 
+
+        if ( location.reviews && location.reviews.length > 0 ) {
+            thisReview = location.reviews.id(req.params.reviewid);
+            if(!thisReview) {
+                sendJsonResponse(res, 404, {
+                    "message": "reviewid not found"
+                });
+            } else {
+                thisReview.author = req.body.author;
+                thisReview.rating = req.body.rating;
+                thisReview.reviewText = req.body.reviewText;
+                location.save(function(err, location) {
+                    if( err ) {
+                        sendJsonResponse( res, 404, err );
+                    } else {
+                        updateAverageRating(location._id);
+                        sendJsonResponse(res, 200, thisReview);
+                    }
+                });
+            }
+        } else {
+            sendJsonResponse(res, 404, {
+                "message": "No review to update"
+            });
+        }
+    });
 }
+
 //router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
 module.exports.reviewsDeleteOne = function(req,res) {
     sendJsonResponse(res, 200, {"status" : "success"});
