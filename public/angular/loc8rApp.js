3d91fec5c9b4f7c2534a9617e302719461c0adca
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-15 11:45:05 -0400

    To do this, we’ll use the module getter syntax at the very bottom of the loc8rApp.js file, after the controller code, as shown in the following code snippet:

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -19,3 +19,7 @@ var locationListCtrl = function ($scope) {
             _id: '5370a35f2536f6785f8dfb6a'
         }]};
 };
+
+angular
+ .module('loc8rApp')
+ .controller('locationListCtrl', locationListCtrl);
