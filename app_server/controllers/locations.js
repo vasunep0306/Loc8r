0722cd20906a133b15d49fd9f7128f64b7b8bac0
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-15 09:05:04 -0400

    doAddReview controller used to post review data to the API

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -6,6 +6,32 @@ var apiOptions = {
 
 
 
+var getLocationInfo = function (req, res, callback) {
+    var requestOptions, path;
+    path = "/api/locations/" + req.params.locationid;
+    requestOptions = {
+        url : apiOptions.server + path,
+        method : "GET",
+        json : {}
+    };
+    request(
+        requestOptions,
+        function(err, response, body) {
+            var data = body;
+            if (response.statusCode === 200) {
+                data.coords = {
+                    lng : body.coords[0],
+                    lat : body.coords[1]
+                };
+                callback(req, res, data);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
+        });
+};
+
+
+
 var renderHomepage = function(req, res, responseBody){
     var message;
     if (!(responseBody instanceof Array)) {
@@ -41,6 +67,23 @@ var _formatDistance = function( distance ) {
 };
 
 
+var _showError = function (req, res, status) {
+    var title, content;
+    if (status === 404) {
+        title = "404, page not found";
+        content = "Oh dear. Looks like we can't find this page. Sorry.";
+    } else {
+        title = status + ", something's gone wrong";
+        content = "Something, somewhere, has gone just a little bit wrong.";
+    }
+    res.status(status);
+    res.render('generic-text', {
+        title : title,
+        content : content
+    });
+};
+
+
 var renderDetailPage = function ( req, res, locDetail ) {
     res.render('location-info', {
         title:  locDetail.name,
@@ -54,6 +97,14 @@ var renderDetailPage = function ( req, res, locDetail ) {
         location: locDetail
     });
 }
+
+
+var renderReviewForm = function (req, res, locDetail) {
+    res.render('location-review-form', {
+        title: 'Review ' + locDetail.name + ' on Loc8r',
+        pageHeader: { title: 'Review ' + locDetail.name }
+    });
+};
 
 /* GET 'home' page */
 module.exports.homeList = function(req, res){
@@ -85,30 +136,41 @@ module.exports.homeList = function(req, res){
 
 
 /* GET 'Location info' page */
-module.exports.locationInfo = function(req, res) {
-    var requestOptions, path;
-    path = "/api/locations/" + req.params.locationid;
-    requestOptions = {
-        url : apiOptions.server + path,
-        method : "GET",
-        json : {}
-    };
-    request(
-        requestOptions,
-        function(err, response, body) {
-            var data = body;
-            data.coords = {lng : body.coords[0],lat : body.coords[1]}; 
-            renderDetailPage(req, res, data);
-        }
-    );
+module.exports.locationInfo = function(req, res){
+    getLocationInfo(req, res, function(req, res, responseData) {
+        renderDetailPage(req, res, responseData);
+    });
 };
+
 
 
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-    res.render('location-review-form', {
-        title: 'Review Starcups on Loc8r',
-        pageHeader: { title: 'Review Starcups' }
+    getLocationInfo(req, res, function(req, res, responseData) {
+        renderReviewForm(req, res, responseData);
     });
 };
+
+module.exports.doAddReview = function(req, res) {
+    var requestOptions, path, locationid, postdata;
+    locationid = req.params.locationid;
+    path = "/api/locations/" + locationid + '/reviews';
+    postdata = {
+        author: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        reviewText: req.body.review
+    };
+    requestOptions = {
+        url : apiOptions.server + path,
+        method : "POST",
+        json : postdata
+    };
+    request(requestOptions, function(err, response, body){
+        if (response.statusCode === 201) {
+            res.redirect('/location/' + locationid);
+        } else {
+            _showError(req, res, response.statusCode);
+        }
+    });
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,7 +7,9 @@ var ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.homeList);
 router.get('/location/:locationid', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/:locationid/reviews/new', ctrlLocations.addReview);
+router.post('/location/:locationid/reviews/new', ctrlLocations.doAddReview);
+
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
